4e787be3b9b7087147193115bb436cbe4d3baf06
myOmikron <accounts@omikron.dev>
2021-06-15 21:29:29 +0200

    Added variables

--- web/description/models.py
+++ web/description/models.py
@@ -2,6 +2,27 @@ from django.db import models
 from django.db.models import CharField, ForeignKey
 
 
+class Variable(models.Model):
+    name = CharField(unique=True, max_length=255)
+    value = CharField(blank=True, null=True, default="", max_length=1024)
+
+    def validate(self, variable_name: str) -> bool:
+        """Validates a given variable.
+
+        To return True, the variable has to be a- and prefixed with '#' and is not allowed to have ' ' in it.
+
+        :param variable_name: Name of the variable
+        :type variable_name: str
+        :return: Returns True if a valid variable name is given
+        :rtype: bool
+        """
+        if not variable_name.startswith('#') or not variable_name.endswith('#'):
+            return False
+        if " " in variable_name:
+            return False
+        return True
+
+
 class CheckType(models.Model):
     name = CharField(default="", max_length=255, unique=True)
 
