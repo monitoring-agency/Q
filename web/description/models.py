0f55b4f58ebc3b7fc6572118663dbbe790341cf2
myOmikron <accounts@omikron.dev>
2021-06-15 21:08:21 +0200

    Added checktype to models

--- web/description/models.py
+++ web/description/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from django.db.models import CharField
+from django.db.models import CharField, ForeignKey
+
+
+class CheckType(models.Model):
+    name = CharField(default="", max_length=255, unique=True)
 
 
 class Checks(models.Model):
-    name = CharField(default="", max_length=255, unique=True, blank=False, null=False)
+    name = CharField(default="", max_length=255, unique=True)
     cmd = CharField(default="", max_length=1024, blank=True, null=True)
+    check_type = ForeignKey(CheckType, on_delete=models.DO_NOTHING, blank=True, null=True)
