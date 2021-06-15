0cd4e723875394dd7c27fcc9627d977a8aa803ff
myOmikron <accounts@omikron.dev>
2021-06-15 20:59:47 +0200

    Added model for checks

--- web/description/models.py
+++ web/description/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+
+class Checks(models.Model):
+    name = CharField(default="", max_length=255, unique=True, blank=False, null=False)
+    cmd = CharField(default="", max_length=1024, blank=True, null=True)
