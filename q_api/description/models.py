fcac12ceaab00fc0296e102bf805001bab5644d9
myOmikron <accounts@omikron.dev>
2021-07-16 21:08:12 +0200

    Added globalvar and contact, contactgroups

--- q_api/description/models.py
+++ q_api/description/models.py
@@ -2,13 +2,16 @@ from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelatio
 from django.contrib.contenttypes.models import ContentType
 from django.core.validators import RegexValidator
 from django.db import models
-from django.db.models import CharField, ForeignKey, ManyToManyField, PositiveIntegerField, BooleanField
+from django.db.models import CharField, ForeignKey, ManyToManyField, PositiveIntegerField, BooleanField, EmailField
+
+
+class GlobalVariable(models.Model):
+    """Represents a global variable"""
+    kvp = GenericRelation("GenericKVP")
 
 
 class SchedulingInterval(models.Model):
-    """Scheduling Interval. The value is interpreted in seconds.
-
-    """
+    """Scheduling Interval. The value is interpreted in seconds."""
     interval = models.PositiveIntegerField(default=350)
 
     def __str__(self):
@@ -21,6 +24,13 @@ class Day(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of day can not be \"\"")
+        super(Day, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                              update_fields=update_fields)
 
 
 class Period(models.Model):
@@ -37,7 +47,7 @@ class Period(models.Model):
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         if int(self.stop_time) <= int(self.start_time):
-            raise ValueError("Starttime has to be Stoptime")
+            raise ValueError("start_time has to lesser than stop_time")
         super(Period, self).save(force_insert=force_insert, force_update=force_update, using=using,
                                  update_fields=update_fields)
 
@@ -59,12 +69,38 @@ class TimePeriod(models.Model):
     def __str__(self):
         return self.name
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "time_periods": {
+                x.day.name: [
+                    {"start_time": y.start_time, "stop_time": y.stop_time}
+                    for y in x.periods.all()
+                ] for x in self.time_periods.all()
+            }
+        }
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of a TimePeriod can not be \"\"")
+        super(TimePeriod, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                     update_fields=update_fields)
+
 
 class CheckType(models.Model):
     name = CharField(default="", max_length=255, unique=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of a CheckType can not be \"\"")
+        super(CheckType, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                    update_fields=update_fields)
 
 
 class Check(models.Model):
@@ -75,8 +111,76 @@ class Check(models.Model):
     def __str__(self):
         return self.name
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "cmd": self.name if self.name else "",
+            "check_type": self.check_type.name if self.check_type else ""
+        }
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of a Check can not be \"\"")
+        super(Check, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                update_fields=update_fields)
+
+
+class Contact(models.Model):
+    """Contact represents the model which holds the contact information and the contact methods that should be
+        used when generating an event.
+    """
+    name = CharField(default="", max_length=255, unique=True)
+    mail = EmailField(default="", max_length=255, null=True, blank=True)
+    linked_host_notifications = ManyToManyField(Check, blank=True, related_name="contact_host_check")
+    linked_metric_notifications = ManyToManyField(Check, blank=True, related_name="contact_metric_check")
+    kvp = GenericRelation("GenericKVP")
+
+    def __str__(self):
+        return self.name
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "mail": self.mail,
+            "variables": {y[0]: y[1] for y in (x for x in self.kvp.all())}
+        }
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of a Contact can not be \"\"")
+        super(Contact, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                  update_fields=update_fields)
+
+
+class ContactGroup(models.Model):
+    """This class represents a group of contacts. Only used for grouping"""
+    name = CharField(default="", max_length=255, unique=True)
+    linked_contacts = ManyToManyField(Contact, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "linked_contacts": [x.id for x in self.linked_contacts.all()]
+        }
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of a ContactGroup can not be \"\"")
+        super(ContactGroup, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                       update_fields=update_fields)
+
 
 class HostTemplate(models.Model):
+    """Template of a host"""
     name = CharField(default="", max_length=255, unique=True)
     address = CharField(default="", max_length=255, blank=True, null=True)
     linked_check = ForeignKey(Check, on_delete=models.DO_NOTHING, blank=True, null=True)
@@ -97,9 +201,30 @@ class HostTemplate(models.Model):
     def __str__(self):
         return self.name
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+            "linked_check": self.linked_check_id,
+            "host_templates": [x.id for x in self.host_templates.all()],
+            "scheduling_interval": self.scheduling_interval_id,
+            "scheduling_period": self.scheduling_period_id,
+            "notification_period": self.notification_period_id,
+            "variables": {y[0]: y[1] for y in (x for x in self.kvp.all())}
+        }
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of a HostTemplate can not be \"\"")
+        super(HostTemplate, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                       update_fields=update_fields)
+
 
 class Host(models.Model):
-    name = CharField(default="", max_length=255, unique=True)
+    """This class represents a host"""
+    name = CharField(max_length=255, unique=True)
     address = CharField(default="", max_length=255, blank=True, null=True)
     linked_check = ForeignKey(Check, on_delete=models.DO_NOTHING, blank=True, null=True)
     disabled = BooleanField(default=False, blank=True, null=True)
@@ -120,12 +245,34 @@ class Host(models.Model):
     def __str__(self):
         return self.name
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+            "linked_check": self.linked_check_id,
+            "disabled": self.disabled,
+            "host_templates": [x.id for x in self.host_templates.all()],
+            "scheduling_interval": self.scheduling_interval_id,
+            "scheduling_period": self.scheduling_period_id,
+            "notification_period": self.notification_period_id,
+            "variables": {y[0]: y[1] for y in (x for x in self.kvp.all())}
+        }
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of a Host can not be \"\"")
+        super(Host, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                               update_fields=update_fields)
+
 
 class MetricTemplate(models.Model):
-    name = CharField(default="", max_length=255)
+    """This class represents a template for a metric"""
+    name = CharField(default="", max_length=255, unique=True)
     linked_check = ForeignKey(Check, on_delete=models.DO_NOTHING, blank=True, null=True)
     linked_host = ForeignKey(Host, on_delete=models.CASCADE)
-    metric_template = ManyToManyField("self", blank=True)
+    metric_templates = ManyToManyField("self", blank=True)
     scheduling_interval = ForeignKey(SchedulingInterval, on_delete=models.DO_NOTHING, blank=True, null=True)
     scheduling_period = ForeignKey(
         TimePeriod, on_delete=models.DO_NOTHING,
@@ -142,13 +289,34 @@ class MetricTemplate(models.Model):
     def __str__(self):
         return self.name
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "linked_check": self.linked_check_id if self.linked_check else "",
+            "linked_host": self.linked_host_id if self.linked_host else "",
+            "metric_templates": [x.id for x in self.metric_templates.all()],
+            "scheduling_interval": self.scheduling_interval_id if self.scheduling_interval else "",
+            "scheduling_period": self.scheduling_period_id if self.scheduling_period else "",
+            "notification_period": self.notification_period_id if self.notification_period else "",
+            "variables": {y[0]: y[1] for y in (x.to_dict() for x in self.kvp.all())}
+        }
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if self.name == "":
+            raise ValueError("Name of a MetricTemplate can not be \"\"")
+        super(MetricTemplate, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                         update_fields=update_fields)
+
 
 class Metric(models.Model):
+    """This class represents a Metric"""
     name = CharField(default="", max_length=255)
     linked_check = ForeignKey(Check, on_delete=models.DO_NOTHING, blank=True, null=True)
     linked_host = ForeignKey(Host, on_delete=models.CASCADE)
     disabled = BooleanField(default=False, blank=True, null=True)
-    metric_template = ManyToManyField(MetricTemplate, blank=True)
+    metric_templates = ManyToManyField(MetricTemplate, blank=True)
     scheduling_interval = ForeignKey(SchedulingInterval, on_delete=models.DO_NOTHING, blank=True, null=True)
     scheduling_period = ForeignKey(
         TimePeriod, on_delete=models.DO_NOTHING,
@@ -164,6 +332,20 @@ class Metric(models.Model):
 
     def __str__(self):
         return self.name
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "linked_check": self.linked_check_id if self.linked_check else "",
+            "linked_host": self.linked_host_id if self.linked_host else "",
+            "disabled": self.disabled,
+            "metric_templates": [x.id for x in self.metric_templates.all()],
+            "scheduling_interval": self.scheduling_interval_id if self.scheduling_interval else "",
+            "scheduling_period": self.scheduling_period_id if self.scheduling_period else "",
+            "notification_period": self.notification_period_id if self.notification_period else "",
+            "variables": {y[0]: y[1] for y in (x.to_dict() for x in self.kvp.all())}
+        }
 
 
 class Label(models.Model):
@@ -182,3 +364,6 @@ class GenericKVP(models.Model):
 
     def __str__(self):
         return f"{self.referent} - {self.key}: {self.value}"
+
+    def to_dict(self):
+        return self.key, self.value
