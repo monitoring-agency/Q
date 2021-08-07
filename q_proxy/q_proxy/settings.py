b2ca9413926c6a51437cf59bd92fc4fcba234238
myOmikron <accounts@omikron.dev>
2021-08-08 01:37:41 +0200

    Initialized app api

--- q_proxy/q_proxy/settings.py
+++ q_proxy/q_proxy/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-*_iskoe!5h-r!s$@^y!barzdr%&i-z=kijf*quazmq#m3yje#$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
