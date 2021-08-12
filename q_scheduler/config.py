a8b04c2129030b11a1c9c8235c18cf44d79d4214
myOmikron <accounts@omikron.dev>
2021-08-12 03:24:52 +0200

    Added woker definition to config

--- q_scheduler/config.py
+++ q_scheduler/config.py
@@ -4,4 +4,5 @@ from staticconfig import Config
 class SchedulerConfig(Config):
     def __init__(self):
         super().__init__()
-        self.description_path = "/var/lib/q/declaration.json"
+        self.declaration_path = "/var/lib/q/declaration.json"
+        self.workers = 10
