564f3248d9f54c47c4e6d3eed47dffabb5bcb187
myOmikron <accounts@omikron.dev>
2021-08-17 15:39:53 +0200

    Added migrations and corrected populate command to delete existing entries

--- q_web/api/management/create_proxy.py
+++ q_web/api/management/create_proxy.py
@@ -2,8 +2,12 @@ from description.models import Proxy
 
 
 def create_proxy():
+    if Proxy.objects.filter(name="local").exists():
+        Proxy.objects.get(name="local").delete()
     proxy, _ = Proxy.objects.get_or_create(
         name="local",
         address="127.0.0.1",
         port=8443,
+        web_address="127.0.0.1",
+        web_port=4443
     )
