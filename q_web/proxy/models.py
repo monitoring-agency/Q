4996b9d6a1644f0a835da1dd259a034d0f90ad08
myOmikron <accounts@omikron.dev>
2021-08-18 19:04:08 +0200

    Implemented models for data

--- q_web/proxy/models.py
+++ q_web/proxy/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db.models import Model, CharField, DateTimeField, FloatField, IntegerField, ManyToManyField
 
-# Create your models here.
+
+class DataModel(Model):
+    value = FloatField(default=0)
+
+
+class CheckResult(Model):
+    stdout = CharField(default="", max_length=8192)
+    return_code = IntegerField(default=-1)
+    linked_data = ManyToManyField(DataModel, blank=True)
+    linked_object = IntegerField(default=0)
+    linked_object_type = IntegerField(default=0)
+    meta_process_time = DateTimeField()
+    meta_process_execution_time = FloatField()
