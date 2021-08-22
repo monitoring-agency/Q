81582487576b3848f298fb5330e4e04ed0e970b9
myOmikron <accounts@omikron.dev>
2021-08-22 17:53:44 +0200

    Added secrets to populate section

--- q_web/api/management/create_proxy.py
+++ q_web/api/management/create_proxy.py
@@ -1,13 +1,19 @@
+import secrets
+import string
+
 from description.models import Proxy
 
 
 def create_proxy():
+    alphabet = string.ascii_letters + string.digits + string.punctuation
     if Proxy.objects.filter(name="local").exists():
         Proxy.objects.get(name="local").delete()
     proxy, _ = Proxy.objects.get_or_create(
         name="local",
         address="127.0.0.1",
         port=8443,
+        secret="".join(secrets.choice(alphabet) for _ in range(255)),
         web_address="127.0.0.1",
-        web_port=4443
+        web_port=4443,
+        web_secret="".join(secrets.choice(alphabet) for _ in range(255)),
     )
