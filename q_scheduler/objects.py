5f5e99f5b5405ed94254146dd2ad7cac33e2bd41
myOmikron <accounts@omikron.dev>
2021-08-28 02:38:29 +0200

    Fixed bug where id is converted to tuple

--- q_scheduler/objects.py
+++ q_scheduler/objects.py
@@ -7,7 +7,7 @@ class SchedulingPeriod:
 
 class Check:
     def __init__(self, id, linked_check, scheduling_period, scheduling_interval, context):
-        self.id = id,
+        self.id = id
         self.linked_check = linked_check
         self.scheduling_period = scheduling_period
         self.scheduling_interval = scheduling_interval
