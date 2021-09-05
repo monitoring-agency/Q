a33d739df685eee1b85b10c9d252d5e8b91939db
myOmikron <accounts@omikron.dev>
2021-09-05 19:15:33 +0200

    Registered scheduledobject, checkresult and dataset in admin view

--- q_web/proxy/admin.py
+++ q_web/proxy/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from proxy.models import *
+
+
+@admin.register(ScheduledObject)
+class ScheduledObjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CheckResult)
+class CheckResultAdmin(admin.ModelAdmin):
+    list_display = ("state", "meta_process_end_time")
+
+
+@admin.register(DataSet)
+class DataSetAdmin(admin.ModelAdmin):
+    list_display = ("name", "value")
