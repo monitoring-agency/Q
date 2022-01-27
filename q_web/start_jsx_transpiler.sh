ab765b18a39bb8d1a2b4977d5978dcd2613c2c31
myOmikron <accounts@omikron.dev>
2022-01-27 16:51:19 +0100

    Ignore q_sdk as well as minify the output

--- q_web/start_jsx_transpiler.sh
+++ q_web/start_jsx_transpiler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-npx babel --watch static/js_src --out-dir static/js --presets react-app/prod
+npx babel --watch static/js_src --out-dir static/js --presets react-app/prod --minified --ignore=q_sdk.js
 
