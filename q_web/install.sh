fc51693ea2286892cb9a019b04039a9d7a0b17c3
myOmikron <git@omikron.dev>
2022-02-13 03:44:43 +0100

    Corrected path of q_sdk.js

--- q_web/install.sh
+++ q_web/install.sh
@@ -7,4 +7,4 @@ cp index.html /var/www/html/
 /bin/bash start_jsx_transpiler.sh
 cp -r js/ /var/www/html/static
 mkdir -p /var/www/html/static/utils
-cp js_src/utils/q_sdk.js /var/www/html/static/utils/
+cp js_src/utils/q_sdk.js /var/www/html/static/js/utils/
