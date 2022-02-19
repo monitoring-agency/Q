efbbcd93c8be7774175085e001eb9e83557a4e6a
myOmikron <git@omikron.dev>
2022-02-19 03:18:30 +0100

    Added generateConfiguration command to proxy index

--- q_web/js_src/declaration/proxies/q_proxies_index.js
+++ q_web/js_src/declaration/proxies/q_proxies_index.js
@@ -1,5 +1,7 @@
 import {React, toast} from "../../react.js";
 import ctx from "../../lib/q_ctx.js";
+import Modal from "../../lib/q_modal.js";
+import TextArea from "../../lib/q_textarea.js";
 
 export default class DeclarationProxiesIndexView extends React.Component {
     static contextType = ctx;
@@ -7,7 +9,13 @@ export default class DeclarationProxiesIndexView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            "proxies": []
+            "proxies": [],
+            "renderModal": false,
+            "modal": {
+                "configurationModal": false,
+                "configurationText": "",
+                "declarationModal": false,
+            }
         }
     }
 
@@ -33,9 +41,41 @@ export default class DeclarationProxiesIndexView extends React.Component {
                     <td className="normalCell">{proxy.name}</td>
                     <td className="normalCell">{proxy.address}</td>
                     <td>{proxy.comment}</td>
-                    <td className="normalCell">
+                    <td className="smallCell">
                         <button className="colorless button"
                                 onClick={(v) => {
+                                    v.stopPropagation();
+                                }} >
+                            <img src={this.context.static + "img/share.svg"} alt="Share" />
+                        </button>
+                    </td>
+                    <td className="smallCell">
+                        <button className="colorless button"
+                                onClick={(v) => {
+                                    v.stopPropagation();
+                                    let modal = this.state.modal;
+                                    modal.configurationModal = true;
+                                    modal.declarationModal = false;
+                                    this.setState({
+                                        "renderModal": true,
+                                        "modal": modal
+                                    });
+                                    this.context.sdk.generateProxyConfiguration(proxy.id).then((res) => {
+                                        if(res.status !== 200) {
+                                            toast.error(res.text);
+                                            return;
+                                        }
+                                        modal.configurationText = res.data;
+                                        this.setState({"modal": modal});
+                                    });
+                                }} >
+                            <img src={this.context.static + "img/key.svg"} alt="GenConfig" />
+                        </button>
+                    </td>
+                    <td className="smallCell">
+                        <button className="colorless button"
+                                onClick={(v) => {
+                                    v.stopPropagation();
                                     this.context.sdk.deleteProxy(proxy.id).then((res) => {
                                         if(res.status === 200) {
                                             toast.success("Proxy deleted", {autoClose: 1000});
@@ -44,7 +84,6 @@ export default class DeclarationProxiesIndexView extends React.Component {
                                             toast.error(res.message);
                                         }
                                     });
-                                    v.stopPropagation();
                                 }} >
                             <img src={this.context.static + "img/delete.svg"} alt="Delete" />
                         </button>
@@ -72,6 +111,8 @@ export default class DeclarationProxiesIndexView extends React.Component {
                                 <th>Name</th>
                                 <th>Address</th>
                                 <th>Comment</th>
+                                <th>Update declaration</th>
+                                <th>Generate configuration</th>
                                 <th>Delete</th>
                             </tr>
                             {rows}
@@ -79,7 +120,51 @@ export default class DeclarationProxiesIndexView extends React.Component {
                     </table>;
         }
 
+        let modalContent;
+        if(this.state.modal.declarationModal) {
+
+        } else if(this.state.modal.configurationModal) {
+            if(this.state.modal.configurationText === "") {
+                modalContent = <div className="flexColumn centered">
+                    <div style={{paddingBottom: 2 + "rem"}}>Generating configuration...</div>
+                    <img className="rotate bigImg"
+                         src={this.context.static + "img/loader.svg"}
+                         alt="Loading" />
+                </div>;
+            } else {
+                modalContent = <div className="flexColumn centered">
+                    <div style={{marginBottom: 1 + "rem"}}>Execute this command on the target proxy</div>
+                    <div className="flexRow centered">
+                        <TextArea className="darkInput modalTextArea"
+                                  value={this.state.modal.configurationText}
+                                  readonly={true}
+                                  id="refConfigurationText" />
+                        <img className="buttonImg"
+                             style={{marginLeft: 1 + "rem"}}
+                             src={this.context.static + "img/save.svg"}
+                             alt="Copy"
+                             onClick={(v) => {
+                                 v.stopPropagation();
+                                 document.getElementById("refConfigurationText").select();
+                                 if(window.isSecureContext) {
+                                     navigator.clipboard.writeText(this.state.modal.configurationText).then((res) => {
+                                         toast.info("Copied to clipboard", {autoClose: 1500});
+                                     });
+                                 } else {
+                                     toast.error("Couldn't copy to clipboard without secure context", {autoClose: 1500});
+                                 }
+                             }} />
+                    </div>
+                </div>;
+            }
+        }
+
         return (<div>
+            <Modal content={modalContent}
+                   onClose={() => {
+                       this.setState({"renderModal": false});
+                   }}
+                   show={this.state.renderModal} />
             <div className="declarationContent">
                 <div className="declarationHeader">
                     <div className="flexRow">
