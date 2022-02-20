ff959b274d0d237e6bed38ed228ce9739dafda9a
myOmikron <git@omikron.dev>
2022-02-20 01:41:27 +0100

    Added views for globalvariables

--- q_web/js_src/declaration/globalVariables/q_globalvariable_view.js
+++ q_web/js_src/declaration/globalVariables/q_globalvariable_view.js
@@ -1,5 +1,7 @@
 import {React, toast} from "../../react.js";
 import ctx from "../../lib/q_ctx.js";
+import TextInput from "../../lib/q_input.js";
+import TextArea from "../../lib/q_textarea.js";
 
 
 export default class DeclarationGlobalVariableView extends React.Component {
@@ -8,21 +10,121 @@ export default class DeclarationGlobalVariableView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            "globalvariable": {
+                "key": "",
+                "value": "",
+                "comment": ""
+            },
+            "faultyKey": false,
         }
 
-        this.createGlobalVariable = this.createGlobalVariable.bind(this);
+        this.createOrUpdateGlobalVariable = this.createOrUpdateGlobalVariable.bind(this);
     }
 
-    createGlobalVariable() {
+    checkFaultyKey(value) {
+        let regex = new RegExp("^[^ \$]+$");
+        return !String(value).match(regex);
+    }
 
+    createOrUpdateGlobalVariable() {
+        if(this.checkFaultyKey(this.state.globalvariable.key)) {
+            toast.error("Faulty configuration", {autoClose: 1500});
+            this.setState({"faultyKey": true});
+            return;
+        }
+
+        if("globalvariable_id" in this.props) {
+            this.context.sdk.updateGlobalVariable(this.props.globalvariable_id, this.state.globalvariable).then((v) => {
+                if(v.status === 200) {
+                    toast.success("Global variable has been updated", {autoClose: 1500});
+                    this.context.setPath({"path": ["declaration", "globalvariables", "index"]});
+                } else {
+                    toast.error(v.text);
+                }
+            });
+        } else {
+            this.context.sdk.createGlobalVariable(this.state.globalvariable).then((res) => {
+            if(res.status !== 200) {
+                toast.error(res.text, {autoClose: 1500});
+            } else {
+                toast.success("Global variable was created!", {autoClose: 1500});
+                this.context.setPath({"path": ["declaration", "globalvariables", "index"]});
+            }
+        });
+        }
     }
 
     componentWillMount() {
-
+        if("globalvariable_id" in this.props) {
+            this.context.sdk.getGlobalVariable(this.props.globalvariable_id).then((res) => {
+                if(res.status === 200) {
+                    this.setState({"globalvariable": res.data});
+                }
+            })
+        }
     }
 
     render() {
-        return null;
+        let heading;
+        if("globalvariable_id" in this.props) {
+            heading = <h2 className="declarationHeading">Update GlobalVariable</h2>;
+        } else {
+            heading = <h2 className="declarationHeading">Create new GlobalVariable</h2>;
+        }
+
+        return <div className="declarationContent">
+            {heading}
+            <table className="declarationTable">
+                <tr>
+                    <td>
+                        <label>Key</label>
+                    </td>
+                    <td>
+                        <TextInput className={this.state.faultyKey ? "darkInput redBorder" : "darkInput"}
+                                   value={this.state.globalvariable.key}
+                                   onChange={(v) => {
+                                       let globalVariable = this.state.globalvariable;
+                                       let faulty = this.checkFaultyKey(v);
+                                       globalVariable.key = v;
+                                       this.setState({"globalvariable": globalVariable, "faultyKey": faulty});
+                                   }} />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Value</label>
+                    </td>
+                    <td>
+                        <TextInput className="darkInput"
+                                   value={this.state.globalvariable.value}
+                                   onChange={(v) => {
+                                       let globalVariable = this.state.globalvariable;
+                                       globalVariable.value = v;
+                                       this.setState({"globalvariable": globalVariable});
+                                   }} />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Comment</label>
+                    </td>
+                    <td>
+                        <TextArea className="darkInput bigTextArea"
+                                  value={this.state.globalvariable.comment}
+                                  onChange={(v) => {
+                                      let globalvariable = this.state.globalvariable;
+                                      globalvariable.comment = v;
+                                      this.setState({"globalvariable": globalvariable});
+                                  }} />
+                    </td>
+                </tr>
+            </table>
+            <button className="button"
+                    onClick={(v) => {
+                        this.createOrUpdateGlobalVariable();
+                    }} >
+                Submit
+            </button>
+        </div>
     }
 }
--- q_web/js_src/declaration/globalVariables/q_globalvariables_index.js
+++ q_web/js_src/declaration/globalVariables/q_globalvariables_index.js
@@ -7,15 +7,87 @@ export default class DeclarationGlobalVariableIndexView extends React.Component 
     constructor(props) {
         super(props);
         this.state = {
-
+            "globalvariables": []
         }
     }
 
-    componentDidMount() {
+    updateGlobalVariables() {
+        this.context.sdk.getGlobalVariables().then((v) => {
+            if(v.status === 200) {
+                this.setState({"globalvariables": v.data});
+            }
+        });
+    }
 
+    componentWillMount() {
+        this.updateGlobalVariables();
     }
 
     render() {
-        return null;
+        let rows = [];
+        for(let globalvariable of this.state.globalvariables) {
+            rows.push(<tr className="clickable"
+                          onClick={
+                              this.context.setPath.bind(null, {"path": ["declaration", "globalvariables", globalvariable.id]})
+                          } >
+                <td><input type="checkbox" /></td>
+                <td>{globalvariable.id}</td>
+                <td>{globalvariable.key}</td>
+                <td>{globalvariable.value}</td>
+                <td>{globalvariable.comment}</td>
+                <td>
+                    <button className="colorless button"
+                            onClick={(v) => {
+                                v.stopPropagation();
+                                this.context.sdk.deleteGlobalVariable(globalvariable.id).then((res) => {
+                                    if(res.status === 200) {
+                                        toast.success("Global variable deleted", {autoClose: 1000})
+                                        this.updateGlobalVariables();
+                                    } else {
+                                        toast.error(res.message);
+                                    }
+                                });
+                             }} >
+                        <img src={this.context.static + "img/delete.svg"}
+                             alt="Delete" />
+                    </button>
+                </td>
+            </tr>);
+        }
+
+        let noGlobalVariables;
+        let table;
+        if(this.state.globalvariables.length === 0) {
+            noGlobalVariables = "There are no global variables.";
+        } else {
+            table = <table>
+                <tr>
+                    <th className="smallCell"><input type="checkbox" /></th>
+                    <th className="smallCell">ID</th>
+                    <th>Key</th>
+                    <th>Value</th>
+                    <th>Comment</th>
+                    <th className="normalCell">Delete</th>
+                </tr>
+                {rows}
+            </table>;
+        }
+
+        return <div className="declarationContent">
+                <div className="declarationHeader">
+                    <div className="flexRow">
+                        <button className="buttonLink"
+                                onClick={
+                                    this.context.setPath.bind(null, {"path": ["declaration", "globalvariables", "create"]})
+                                } >
+                            Add Global Variable
+                        </button>
+                    </div>
+                </div>
+                <div className="declarationList">
+                    {noGlobalVariables}
+                    {table}
+                </div>
+            </div>
     }
 }
